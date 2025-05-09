aa2ede3ec43287e1ea75c8d30ebc6cfe716941b7
parsa <parsafarashah@gmail.com>
2025-05-09 18:45:44 +0330

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import React from "react";
+import headerXl from "../../public/images/Desktop and tablet.webp";
+import headerMobile from "../../public/images/mobile.webp";
+import younes from "../../public/images/younesImage.png";
+
+function Header() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center pt-[50px]">
+      <div className="w-full flex justify-center">
+        <Image
+          className="object-contain hidden sm:block max-w-[1200px]  w-full h-full "
+          src={headerXl}
+          alt="haeder Image Xl"
+        />
+        <Image
+          className="object-contain sm:hidden w-full h-full pt-3"
+          src={headerMobile}
+          alt="haeder Image Xl"
+        />
+      </div>
+      <div className="pt-1">
+        <h1 className="text-center font-roboto text-[#282828] pt-6 sm:pt-0 text-[32px] sm:text-[42px] leading-11 sm:leading-13 font-extrabold sm:font-bold">
+          طراحی و توسعه وبسایت در کلاس جهانی <br className="md:hidden block" />
+          <br className="lg:block hidden" /> برای متفاوت دیده شدن
+        </h1>
+        <p className="pt-3 sm:pt-2 text-[14px] font-inter font-normal text-[#717171] leading-6 text-center">
+          تیم زمین آمده است تا برای طراحی و توسعه یک وبسایت دوستانه و مدرن در
+          کنار شما
+          <br />
+          باشد تا بتوانید از رقبای خود متمایز دیده شوید
+        </p>
+      </div>
+      {/* button */}
+      <div className="w-full flex justify-center">
+        <div className="w-full max-w-[1400px] flex items-center justify-between gap-2 pt-8 sm:pt-4">
+          <div className="bg-[#BFBFBF] w-full h-0.5"></div>
+          <div className="flex gap-3">
+            <div className="w-full flex justify-center">
+              <button className="w-[131px] cursor-pointer flex  justify-center text-sm items-center h-9 text-white bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 hover:bg-gradient-to-br shadow-md shadow-gray-100/20 dark:shadow-md dark:shadow-gray-300/80 font-medium rounded-lg text-center font-inter ">
+                اطلاعات بیشتر
+              </button>
+            </div>
+            <div className="w-full flex justify-center">
+              <button className="w-[131px] cursor-pointer flex justify-center items-center h-9 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br shadow-md shadow-blue-100/20 dark:shadow-md dark:shadow-blue-300/80 font-medium rounded-lg text-sm text-center font-inter">
+                ثبت درخواست
+              </button>
+            </div>
+          </div>
+          <div className="bg-[#BFBFBF] w-full h-0.5"></div>
+        </div>
+      </div>
+      {/* 4image  */}
+      <div className="flex flex-row-reverse justify-center pt-7 sm:pt-5 pr-[40px]">
+        <div className="z-40 bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+          <Image
+            className="rounded-2xl"
+            alt="imageYounes"
+            src={younes}
+            layout="responsive"
+          />
+        </div>
+        <div className="z-30 relative left-[12px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+          <Image
+            className="rounded-2xl"
+            src={younes}
+            alt="imageYounes"
+            layout="responsive"
+          />
+        </div>
+        <div className="z-20 relative left-[24px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+          <Image
+            className="rounded-2xl"
+            src={younes}
+            alt="imageYounes"
+            layout="responsive"
+          />
+        </div>
+        <div className="z-10 relative left-[36px] bg-amber-800 w-[36px] h-[36px] rounded-[24px] border-2 border-[#F5F5F5]">
+          <Image
+            className="rounded-2xl"
+            src={younes}
+            alt="imageYounes"
+            layout="responsive"
+          />
+        </div>
+      </div>
+
+      {/* end 4image */}
+      <div className="flex justify-center items-center gap-2 pt-3">
+        <p className="font-inter text-[12px] font-medium"> آماده به کار</p>
+        <span className=" relative flex size-2">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex size-2 rounded-full bg-green-600"></span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logoZamin from "../../public/images/logoZamin.svg";
+import Image from "next/image";
+
+function Navbar() {
+  return (
+    <div className="flex justify-center">
+      <div
+        className="bg-white w-[361px] md:w-[537px] h-[56px] fixed top-[16px] rounded-[19px] shadow flex justify-between px-[12px] py-5 items-center  z-60
+"
+      >
+        <div className="w-[86px] h-[36px] bg-[#F6F8FA] rounded-xl flex justify-around items-center  ">
+          <button className="bg-[#FFFFFF] w-[37px] h-[28px] rounded-md shadow-2xs">
+            فا
+          </button>
+          <button>EN</button>
+        </div>
+        <div className="text-gray-500 hidden md:flex">
+          <ul className="flex justify-between gap-[16px] text-sm font-inter cursor-pointer">
+            <li className="hover:text-black">خدمات</li>
+            <li className="hover:text-black">نمونه ها</li>
+            <li className="hover:text-black">قیمت</li>
+            <li className="hover:text-black">درباره ما</li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-3 ">
+          <h1 className="text-black font-bold font-inter">زمین</h1>
+          <Image src={logoZamin} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

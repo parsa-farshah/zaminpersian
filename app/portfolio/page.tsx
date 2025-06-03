bed8c6aaae97aeb4b254fffc086f226499bc8589
parsa <parsafarashah@gmail.com>
2025-06-03 23:48:31 +0330

    new page 4

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -7,10 +7,6 @@ function page() {
   return (
     <div>
       <div className="relative bg-[#F5F5F5] flex flex-col justify-center px-4 md:px-[90px] xl:px-[357px]">
-        <div
-          className="w-full absolute top-0 left-0 h-screen bg-cover bg-center z-0"
-          style={{ backgroundImage: "url('/images/bg-portfolio2.png')" }}
-        ></div>
         <NavbarPort />
         <PortfolioRecent />
       </div>
